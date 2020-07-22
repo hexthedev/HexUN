23fa2471f2ee2bd163bcb68b8374a095e84c9638
James McCafferty <jamesmccafferty@live.ca>
2020-07-22 15:44:36 -0400

    Added rotator mono

---
+++ Runtime/Utilities/UsefulMonos/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace HexUN
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField]
+        public Vector3 EulerRotation;
+
+        private void FixedUpdate()
+        {
+            transform.rotation *= Quaternion.Euler(EulerRotation * Time.fixedDeltaTime);   
+        }
+    }
+}
