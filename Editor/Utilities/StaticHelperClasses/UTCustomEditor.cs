469f2f542979366525841db159fea70be5b373f1
James McCafferty <jamesmccafferty@live.ca>
2020-07-23 19:20:29 -0400

    Added custom editor utilities

---
+++ Editor/Utilities/StaticHelperClasses/UTCustomEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace HexUN.Edit
+{
+    public static class UTCustomEditor
+    {
+        /// <summary>
+        /// Show disabled reference to a mono behaviour object in custom editor
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static void ShowMonoReference(MonoBehaviour mb)
+        {
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour(mb), typeof(MonoBehaviour), false);
+            GUI.enabled = true;
+        }
+
+        /// <summary>
+        /// Show disabled reference to a scriptable object object in custom editor
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static void ShowSOReference(ScriptableObject so)
+        {
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField("Script", MonoScript.FromScriptableObject(so), typeof(ScriptableObject), false);
+            GUI.enabled = true;
+        }
+
+        /// <summary>
+        /// Show disabled reference to a scriptable object object in custom editor
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static void Header(string label)
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField(label, EditorStyles.boldLabel);
+        }
+    }
+}
