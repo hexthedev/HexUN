4fd7da349f98d2fcf0795d1caeaac3543af6adfa
James McCafferty <jamesmccafferty@live.ca>
2020-07-26 20:46:35 -0400

    Added smmal utility for editors

---
+++ Editor/Utilities/StaticHelperClasses/UTEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace HexUN.Edit
+{
+    /// <summary>
+    /// Super general utilites to do with everything Unity Editor
+    /// </summary>
+    public static class UTEditor
+    {
+        private static Vector2 _toggleSizeCache;
+
+        /// <summary>
+        /// Gets the size of a basic regular toggle in unity
+        /// </summary>
+        public static Vector2 ToggleSize
+        {
+            get
+            {
+                if (_toggleSizeCache == default) _toggleSizeCache = GUI.skin.toggle.CalcSize(GUIContent.none);
+                return _toggleSizeCache;
+            }
+        }
+    }
+}
