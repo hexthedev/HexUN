edbdba5020cc35304096ea83bd0777f500498baf
James McCafferty <jamesmccafferty@live.ca>
2020-09-01 01:55:59 -0400

    worked on convention tools

---
+++ Editor/Data/Convention.cs
@@ -0,0 +1,38 @@
+﻿using HexCS.Data.Persistence;
+using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace HexUN.Data
+{
+    public static class Convention
+    {
+        public static Dictionary<string, ConventionArgs> ConventionArgs
+        {
+            get
+            {
+                UnityPath conventionPath = "Assets/Data/Yaml/config/convention.yaml";
+
+                if (!conventionPath.AbsolutePath.TryAsFileInfo(out FileInfo file)) return null;
+                if (!file.Exists) return null;
+
+                Deserializer des = new Deserializer();
+                Dictionary<string, ConventionArgs> dict = des.Deserialize<Dictionary<string, ConventionArgs>>(file.ReadAllText());
+                return dict;
+
+            }
+        }
+
+        public static ConventionArgs GetConvention(string conv)
+        {
+            if (!ConventionArgs.TryGetValue(conv, out ConventionArgs args)) return null;
+            return args;
+        }
+    }
+
+    public class ConventionArgs
+    {
+        public string location;
+        public string convention;
+    }
+}
