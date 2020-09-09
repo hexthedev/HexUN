7da37bbae673b50eab34cd8097006bf26faf265a
James McCafferty <jamesmccafferty@live.ca>
2020-09-08 22:59:08 -0400

    Added camera rig

---
+++ Runtime/Camera/CameraRig.cs
@@ -0,0 +1,26 @@
+﻿using HexUN.MonoB;
+using UnityEngine;
+
+namespace RoboArena
+{
+    public class CameraRig : AMonoSingletonScene<CameraRig>
+    {
+        [SerializeField]
+        [Tooltip("Where to put camera on destroy")]
+        private Transform _onDestroyParent = null;
+
+        private Camera _managedCam;
+
+        private void Start()
+        {
+            _managedCam = Camera.main;
+            _managedCam.transform.SetParent(transform, false);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if(_onDestroyParent != null && _managedCam != null) _managedCam.transform.SetParent(null);
+        }
+    }
+}
