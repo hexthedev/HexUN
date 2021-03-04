d0f8928bed5bfb36fea165daa8656afe19a4edc2
james <jamesmccafferty@live.ca>
2021-03-04 14:22:36 -0500

    Added UTTransform

---
+++ Runtime/Scripts/Engine/Utilities/StaticHelperClasses/UTTransform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace HexUN.Engine
+{
+    /// <summary>
+    /// Useful helper classes for transforms
+    /// </summary>
+    public static class UTTransform
+    {
+        /// <summary>
+        /// Sets the pos, rot and scale of the transform in local space
+        /// </summary>
+        public static void Set(this Transform target, Vector3 pos, Quaternion rot, Vector3 scale)
+        {
+            target.position = pos;
+            target.localRotation = rot;
+            target.localScale = scale;
+        }
+    }
+}
