31353443f772a486646db21415a43a1adf7f5892
james <jamesmccafferty@live.ca>
2021-06-06 18:08:31 -0400

    Improved request pattern

--- Runtime/Scripts/Framework/Requests/Request.cs
+++ Runtime/Scripts/Framework/Requests/Request.cs
@@ -13,16 +13,30 @@ namespace HexUN.Framework.Request
     /// </summary>
     public class Request<TReq, TRes>
     {
-        private System.Timers.Timer aTimer;
-
         private readonly TaskCompletionSource<TRes> _src = new TaskCompletionSource<TRes>();
-
-        private Coroutine _timeout;
+        
+        private object _repsonderObjectLock = new object();
+        private object _responderObject = null;
 
         /// <summary>
         /// Is the request complete
         /// </summary>
-        public bool IsComplete { get; private set; }
+        public bool IsComplete => State == EState.Complete;
+
+        /// <summary>
+        /// Has the request been claimed by a responder
+        /// </summary>
+        public bool IsClaimed => State != EState.AwaitingClaim;
+
+        /// <summary>
+        /// The current state of the request
+        /// </summary>
+        public EState State { get; private set; } = EState.AwaitingClaim;
+
+        /// <summary>
+        /// Id left by responder object when claiming the request
+        /// </summary>
+        public string Responder { get; private set; }
 
         /// <summary>
         /// The object sent as the request
@@ -43,9 +57,8 @@ namespace HexUN.Framework.Request
         /// Await the completion of the request. Set timeout in milliseconds.
         /// Throws <see cref="TimeoutException"/> if timeout occurs
         /// </summary>
-        public async Task<TRes> AwaitAsync(float timeout = 1000)
+        public async Task<TRes> AwaitAsync()
         {
-            SetTimeoutTimer(timeout);
             return await _src.Task;
         }
 
@@ -59,28 +72,63 @@ namespace HexUN.Framework.Request
         }
 
         /// <summary>
-        /// Complete the request by providing a response
+        /// Claim the request. An object that claims a request is responsible for completeing the
+        /// request and called respond. Retursn false if the claim fails. 
         /// </summary>
-        public void Respond(TRes Response)
+        public bool TryClaim(object responderObject, string responderId)
         {
-            if (IsComplete) return;
+            if (IsClaimed) return false;
 
-            ResponseObject = Response;
-            _src.TrySetResult(Response);
+            lock (_repsonderObjectLock)
+            {
+                if (_responderObject != null) return false;
+
+                _responderObject = responderObject;
+                Responder = responderId;
+                State = EState.AwaitingResponse;
+                return true;
+            }
         }
 
-        private void SetTimeoutTimer(float timeout)
+        /// <summary>
+        /// Complete the request by providing a response.
+        /// </summary>
+        public void Respond(TRes response, object responderObject)
         {
-            aTimer = new Timer(timeout);
-            aTimer.Elapsed += OnTimeout;
-            aTimer.Enabled = true;
+            if(State == EState.AwaitingClaim)
+                throw new RequestException("Attempted to respond without claiming the request. Response failed");
 
-            void OnTimeout(System.Object source, ElapsedEventArgs e)
-            {
-                _src.SetException(new RequestException($"Request timed out after {timeout} milliseconds"));
-                aTimer.Dispose();
-                aTimer = null;
-            }
+            if(State == EState.AwaitingResponse && _responderObject != responderObject)
+                throw new RequestException($"{responderObject} attempted to respond to the request, but is not the registered responder {_responderObject}");
+
+            if(State == EState.Complete)
+                throw new RequestException($"Request is already complete, cannot respond again");
+
+            ResponseObject = response;
+            State = EState.Complete;
+            _src.TrySetResult(response);
+        }
+
+        /// <summary>
+        /// Requests await a claimer when created. Any object can claim the request, then
+        /// they become responsible for completing it
+        /// </summary>
+        public enum EState
+        {
+            /// <summary>
+            /// No responder has yet claimed the request, and no one is reponding
+            /// </summary>
+            AwaitingClaim, 
+
+            /// <summary>
+            /// A responder has claimed the request and is in the process of responding
+            /// </summary>
+            AwaitingResponse,
+
+            /// <summary>
+            /// The request has been completed by the responder
+            /// </summary>
+            Complete
         }
 
         public class RequestException : Exception 
