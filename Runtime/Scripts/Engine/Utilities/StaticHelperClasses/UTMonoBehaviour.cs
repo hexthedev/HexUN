a1bf1e8c33068d02e1071cb3434e0365694e4e68
james <jamesmccafferty@live.ca>
2021-06-21 23:23:08 -0400

    updated monobehaviour helper

--- Runtime/Scripts/Engine/Utilities/StaticHelperClasses/UTMonoBehaviour.cs
+++ Runtime/Scripts/Engine/Utilities/StaticHelperClasses/UTMonoBehaviour.cs
@@ -10,8 +10,7 @@ namespace HexUN.Engine.Utilities
         /// <summary>
         /// Adds a child to the monobehaviours gameobject of type TChild 
         /// </summary>
-        public static TChild AddChild<TParent, TChild>(this TParent monoBehaviour, string name)
-            where TParent: MonoBehaviour
+        public static TChild AddChild<TChild>(this MonoBehaviour monoBehaviour, string name)
             where TChild: MonoBehaviour
         {
             return monoBehaviour.gameObject.AddChild<TChild>(name);
@@ -20,8 +19,7 @@ namespace HexUN.Engine.Utilities
         /// <summary>
         /// Adds a child to the monobehaviours gameobject of type TChild 
         /// </summary>
-        public static void AddChild<TParent, TChild>(this TParent monoBehaviour, string name, out TChild child)
-            where TParent : MonoBehaviour
+        public static void AddChild<TChild>(this MonoBehaviour monoBehaviour, string name, out TChild child)
             where TChild : MonoBehaviour
         {
             child = monoBehaviour.gameObject.AddChild<TChild>(name);
